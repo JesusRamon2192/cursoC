e184da3a22813beae3411a5848e0d84b9f0ba5a0
jesusRamon <jesus.ramon2192@gmail.com>
2023-04-01 10:04:55 -0600

    Clase 7 Estructura de un programa C

---
+++ hello.c
@@ -0,0 +1,10 @@
+#include <stdio.h> 
+//Zona donde incluimos nuestras librerias. 
+//Directivas de precompilador (Preprocessors Comands).
+
+int main()  //Funcion principal, aqui es donde la ejecucion de mi programa comienza. Todo el codigo va aqui dentro.
+{
+    /*First hello world in C language*/
+    printf("Hola Mundo"); //=> imprime en terminal
+    return 0; //=> return 0 termina la funcion. Siempre que ponga 0 en un return dentro de una funcion main, va a cerrar dicha funcion
+}
