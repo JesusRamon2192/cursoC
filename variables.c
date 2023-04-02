5eaa3d97f2812d2728371b68be7a35dd315b7180
jesusRamon <jesus.ramon2192@gmail.com>
2023-04-01 19:13:50 -0600

    Declaracion de variables

---
+++ variables.c
@@ -0,0 +1,19 @@
+#include <stdio.h> 
+
+//Variable declarations
+int a, b, c;
+
+float f, g, h;
+
+int main() 
+{
+    //Variable initialization
+    a = 10;
+    b = 2147483647;
+
+    c = a + b;
+
+
+    printf("Value of sum is: %d\n", c);
+    return 0;
+}
